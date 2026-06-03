4ac31179b99e58d7ddd5b4fe025d9bddc4f59976
adi-1738 <adityamithunr@gmail.com>
2026-06-03 16:52:55 +0200

    Implement watchdog using diagnostic_updater

--- src/vehicle_watchdog/src/watchdog_node.cpp
+++ src/vehicle_watchdog/src/watchdog_node.cpp
@@ -2,204 +2,147 @@
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/PointCloud.h>
 
-// Velocity
+#include <diagnostic_updater/diagnostic_updater.h>
+#include <diagnostic_updater/publisher.h>
 
-int velocity_message_count = 0;
-ros::Time velocity_start_time;
+diagnostic_updater::Updater* updater;
 
-// Lidar
+// Expected frequencies
+double velocity_min = 47.0;
+double velocity_max = 53.0;
 
-int lidar_message_count = 0;
-ros::Time lidar_start_time;
+double lidar_min = 9.0;
+double lidar_max = 11.0;
 
-// Stereo
+double stereo_min = 27.0;
+double stereo_max = 33.0;
 
-int stereo_message_count = 0;
-ros::Time stereo_start_time;
+double slam_min = 9.0;
+double slam_max = 11.0;
 
-// SLAM
-
-int slam_message_count = 0;
-ros::Time slam_start_time;
-
+// Diagnostics
+diagnostic_updater::TopicDiagnostic* velocity_diag;
+diagnostic_updater::TopicDiagnostic* lidar_diag;
+diagnostic_updater::TopicDiagnostic* stereo_diag;
+diagnostic_updater::TopicDiagnostic* slam_diag;
 
 void velocityCallback(
 const nav_msgs::Odometry::ConstPtr& msg)
 {
-    velocity_message_count++;
-
-    ros::Time current_time = ros::Time::now();
-
-    if(velocity_start_time.isZero())
-    {
-        velocity_start_time = current_time;
-    }
-
-    double elapsed_time =
-    (current_time - velocity_start_time).toSec();
-
-    if(elapsed_time >= 1.0)
-    {
-        double frequency =
-        velocity_message_count / elapsed_time;
-
-        if(frequency < 47.0)
-        {
-            ROS_WARN("[LOW] Velocity: %.2f Hz",
-                     frequency);
-        }
-        else
-        {
-            ROS_INFO("[OK] Velocity: %.2f Hz",
-                     frequency);
-        }
-
-        velocity_message_count = 0;
-        velocity_start_time = current_time;
-    }
+    velocity_diag->tick(msg->header.stamp);
 }
-
 
 void lidarCallback(
 const sensor_msgs::PointCloud::ConstPtr& msg)
 {
-    lidar_message_count++;
-
-    ros::Time current_time = ros::Time::now();
-
-    if(lidar_start_time.isZero())
-    {
-        lidar_start_time = current_time;
-    }
-
-    double elapsed_time =
-    (current_time - lidar_start_time).toSec();
-
-    if(elapsed_time >= 1.0)
-    {
-        double frequency =
-        lidar_message_count / elapsed_time;
-
-        if(frequency < 9.0)
-        {
-            ROS_WARN("[LOW] Lidar: %.2f Hz",
-                     frequency);
-        }
-        else
-        {
-            ROS_INFO("[OK] Lidar: %.2f Hz",
-                     frequency);
-        }
-
-        lidar_message_count = 0;
-        lidar_start_time = current_time;
-    }
+    lidar_diag->tick(msg->header.stamp);
 }
 
 void stereoCallback(
 const sensor_msgs::PointCloud::ConstPtr& msg)
 {
-    stereo_message_count++;
-
-    ros::Time current_time = ros::Time::now();
-
-    if(stereo_start_time.isZero())
-    {
-        stereo_start_time = current_time;
-    }
-
-    double elapsed_time =
-    (current_time - stereo_start_time).toSec();
-
-    if(elapsed_time >= 1.0)
-    {
-        double frequency =
-        stereo_message_count / elapsed_time;
-
-        if(frequency < 27.0)
-        {
-            ROS_WARN("[LOW] Stereo: %.2f Hz",
-                     frequency);
-        }
-        else
-        {
-            ROS_INFO("[OK] Stereo: %.2f Hz",
-                     frequency);
-        }
-
-        stereo_message_count = 0;
-        stereo_start_time = current_time;
-    }
+    stereo_diag->tick(msg->header.stamp);
 }
 
 void slamCallback(
 const sensor_msgs::PointCloud::ConstPtr& msg)
 {
-    slam_message_count++;
-
-    ros::Time current_time = ros::Time::now();
-
-    if(slam_start_time.isZero())
-    {
-        slam_start_time = current_time;
-    }
-
-    double elapsed_time =
-    (current_time - slam_start_time).toSec();
-
-    if(elapsed_time >= 1.0)
-    {
-        double frequency =
-        slam_message_count / elapsed_time;
-
-        if(frequency < 9.0)
-        {
-            ROS_WARN("[LOW] SLAM: %.2f Hz",
-                     frequency);
-        }
-        else
-        {
-            ROS_INFO("[OK] SLAM: %.2f Hz",
-                     frequency);
-        }
-
-        slam_message_count = 0;
-        slam_start_time = current_time;
-    }
+    slam_diag->tick(msg->header.stamp);
 }
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "watchdog_node");
+    updater = new diagnostic_updater::Updater();
+    updater->setHardwareID("vehicle_watchdog");
 
     ros::NodeHandle nh;
+
+    updater->setHardwareID("vehicle_watchdog");
+
+    velocity_diag =
+    new diagnostic_updater::TopicDiagnostic(
+        "/estimation/velocity",
+        *updater,
+        diagnostic_updater::FrequencyStatusParam(
+            &velocity_min,
+            &velocity_max,
+            0.1,
+            10),
+        diagnostic_updater::TimeStampStatusParam());
+
+    lidar_diag =
+    new diagnostic_updater::TopicDiagnostic(
+        "/lidar/cone_position_cloud",
+        *updater,
+        diagnostic_updater::FrequencyStatusParam(
+            &lidar_min,
+            &lidar_max,
+            0.1,
+            10),
+        diagnostic_updater::TimeStampStatusParam());
+
+    stereo_diag =
+    new diagnostic_updater::TopicDiagnostic(
+        "/stereo_cone_perception/cones",
+        *updater,
+        diagnostic_updater::FrequencyStatusParam(
+            &stereo_min,
+            &stereo_max,
+            0.1,
+            10),
+        diagnostic_updater::TimeStampStatusParam());
+
+    slam_diag =
+    new diagnostic_updater::TopicDiagnostic(
+        "/slam/map",
+        *updater,
+        diagnostic_updater::FrequencyStatusParam(
+            &slam_min,
+            &slam_max,
+            0.1,
+            10),
+        diagnostic_updater::TimeStampStatusParam());
+
     ros::Subscriber velocity_sub =
-nh.subscribe("/estimation/velocity",
-             100,
-             velocityCallback);
+    nh.subscribe("/estimation/velocity",
+                 100,
+                 velocityCallback);
 
-ros::Subscriber lidar_sub =
-nh.subscribe("/lidar/cone_position_cloud",
-             100,
-             lidarCallback);
+    ros::Subscriber lidar_sub =
+    nh.subscribe("/lidar/cone_position_cloud",
+                 100,
+                 lidarCallback);
 
-ros::Subscriber stereo_sub =
-nh.subscribe("/stereo_cone_perception/cones",
-             100,
-             stereoCallback);
+    ros::Subscriber stereo_sub =
+    nh.subscribe("/stereo_cone_perception/cones",
+                 100,
+                 stereoCallback);
 
-ros::Subscriber slam_sub =
-nh.subscribe("/slam/map",
-             100,
-             slamCallback);
-             
-               ROS_INFO("Vehicle watchdog started");
+    ros::Subscriber slam_sub =
+    nh.subscribe("/slam/map",
+                 100,
+                 slamCallback);
 
-    ros::spin();
+    ROS_INFO("Vehicle watchdog started");
+
+    ros::Rate rate(10);
+
+    while(ros::ok())
+    {
+        ros::spinOnce();
+
+        updater->update();
+
+        rate.sleep();
+    }
+
+    delete velocity_diag;
+    delete lidar_diag;
+    delete stereo_diag;
+    delete slam_diag;
 
     return 0;
-             
-             }
-
+}
 
 
